acc90b2a5f18d5b8f5b292567638e233d732909e
nascimento <nascimento.vinicius32@gmail.com>
2020-04-25 23:36:00 -0400

    working in data

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,41 @@
+import { Router, response, request } from 'express';
+import { startOfHour, parseISO } from 'date-fns';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+
+const appointmentsRouter = Router();
+const appointmentsRepository = new AppointmentsRepository();
+
+appointmentsRouter.get('/', (request, response) => {
+  const appointments = appointmentsRepository.all();
+
+  return response.json(appointments);
+});
+
+
+appointmentsRouter.post('/', (request, response) => {
+  const { provider, date } = request.body;
+
+  const parsedDate = startOfHour(parseISO(date));
+
+  // const findAppointmentInSameDate = appointments.find(appointment =>
+  //   isEqual(parsedDate, appointment.date),
+  // );
+
+  const findAppointmentInSameDate = appointmentsRepository.findByDate(
+    parsedDate,
+  );
+
+  if (findAppointmentInSameDate) {
+    return response
+      .status(400)
+      .json({ message: 'This appointment is already booked' });
+  }
+  const appointment = appointmentsRepository.create({
+    provider,
+    date: parsedDate,
+  });
+
+  return response.json(appointment);
+});
+
+export default appointmentsRouter;
