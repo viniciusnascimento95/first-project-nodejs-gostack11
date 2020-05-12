e816e2584f00226da255587313dde80da30ffd9b
nascimento <nascimento.vinicius32@gmail.com>
2020-05-12 00:27:38 -0400

    create migrations users

---
+++ src/database/migrations/1589257417638-CreateUsers.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export default class CreateUsers1589257417638 implements MigrationInterface {
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+  }
+
+}
